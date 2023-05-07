001e8aebdb42006a5f17ab6b4af6baa34a1e639c
Pedry123 <105668982+Pedry123@users.noreply.github.com>
2023-05-07 11:40:20 -0400

    insertion1

--- bubblesort.h
+++ bubblesort.h
@@ -1,4 +1,5 @@
-void bubblesort(LogRecord* vetor);
+void bubblesort_ascending(LogRecord* vetor);
+void bubblesort_descending(LogRecord* vetor);
 void ascending(LogRecord* vetor, int j);
 void descending(LogRecord* vetor, int j);
 void swap(LogRecord* vetor, int i, int j);
--- main.c
+++ main.c
@@ -5,9 +5,79 @@
 #include"comparators.c"
 #include"bubblesort.c"
 
-void insertionsort() {
-    
+int compareLogRecords(const LogRecord a, const LogRecord b) {
+    if (a.process_id != b.process_id) {
+        return a.process_id - b.process_id;
+    }
+    if (a.year != b.year) {
+        return a.year - b.year;
+    }
+    if (a.month != b.month) {
+        return a.month - b.month;
+    }
+    if (a.day != b.day) {
+        return a.day - b.day;
+    }
+    if (a.hour != b.hour) {
+        return a.hour - b.hour;
+    }
+    if (a.minute != b.minute) {
+        return a.minute - b.minute;
+    }
+    return a.second - b.second;
 }
+
+void insertionsort(LogRecord* vetor) {
+    for (int i = 1; i < NUM_LOG_RECORDS; i++) {
+        LogRecord aux = vetor[i];
+        int j = i - 1;
+        while (j >= 0 && compareLogRecords(vetor[j], aux) > 0) {
+            vetor[j + 1] = vetor[j];
+            j--;
+        }
+        vetor[j + 1] = aux;
+    } 
+}
+
+/*
+void insertionsort(LogRecord* vetor) {
+    for (int i = 1; i < NUM_LOG_RECORDS; i++) {
+        LogRecord aux = vetor[i];
+        int j = i - 1;
+        if (greater(vetor[j].process_id, aux.process_id)) {
+            while (j >= 0) {
+                vetor[j + 1] = vetor[j];
+                j--;
+            }
+        }
+        while (j >= 0 && equal(vetor[j].process_id, aux.process_id) && greater(vetor[j].year, aux.year)) {
+            vetor[j + 1] = vetor[j];
+            j--;
+        }
+        while (j >= 0 && equal(vetor[j].process_id, aux.process_id) && equal(vetor[j].year, aux.year) && greater(vetor[j].month, aux.month)) {
+            vetor[j + 1] = vetor[j];
+            j--;
+        }
+        while (j >= 0 && equal(vetor[j].process_id, aux.process_id) && equal(vetor[j].year, aux.year) && equal(vetor[j].month, aux.month) && greater(vetor[j].day, aux.day)) {
+            vetor[j + 1] = vetor[j];
+            j--;
+        }
+        while (j >= 0 && equal(vetor[j].process_id, aux.process_id) && equal(vetor[j].year, aux.year) && equal(vetor[j].month, aux.month) && equal(vetor[j].day, aux.day) && greater(vetor[j].hour, aux.hour)) {
+            vetor[j + 1] = vetor[j];
+            j--;
+        }
+        while (j >= 0 && equal(vetor[j].process_id, aux.process_id) && equal(vetor[j].year, aux.year) && equal(vetor[j].month, aux.month) && equal(vetor[j].day, aux.day) && equal(vetor[j].hour, aux.hour) && greater(vetor[j].minute, aux.minute)) {
+            vetor[j + 1] = vetor[j];
+            j--;
+        }
+        while (j >= 0 && equal(vetor[j].process_id, aux.process_id) && equal(vetor[j].year, aux.year) && equal(vetor[j].month, aux.month) && equal(vetor[j].day, aux.day) && equal(vetor[j].hour, aux.hour) && equal(vetor[j].minute, aux.minute) && greater(vetor[j].second, aux.second)) {
+            vetor[j + 1] = vetor[j];
+            j--;
+        }
+        vetor[j + 1] = aux;
+    } 
+}
+*/
 
 int main() {
     srand(time(NULL));
@@ -48,6 +118,7 @@ int main() {
         printf("\n\n");
     }
 
+    insertionsort(vetor);
     //bubblesort_descending(vetor);
     printf("Agora ordenado:\n");
 
